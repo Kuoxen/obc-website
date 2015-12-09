35e7f8e97dd876c75e824e4dbb416465ec3810f4
Ben Smith <benjsmi@us.ibm.com>
2015-12-09 12:12:48 -0500

    Init the build script.

---
+++ idsBuild.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# This script is used to build the Docker container for the website
+# using IBM DevOps Services (see https://hub.jazz.net for more info).
+#
+
+echo Setting up Docker...
+mkdir dockercfg ; cd dockercfg
+echo -e $KEY > key.pem
+echo -e $CA_CERT > ca.pem
+echo -e $CERT > cert.pem
+cd ..
+wget http://security.ubuntu.com/ubuntu/pool/main/a/apparmor/libapparmor1_2.8.95~2430-0ubuntu5.3_amd64.deb -O libapparmor.deb
+sudo dpkg -i libapparmor.deb
+rm libapparmor.deb
+wget https://get.docker.com/builds/Linux/x86_64/docker-1.9.1 --quiet -O docker
+chmod +x docker
+
+./docker build -t obc-website .
+./docker stop -t 0 obc-website || true
+./docker rm obc-website || true
+./docker run -p 80:80 -e MONGO_URI=$MONGO_URI -d --name=obc-website obc-website
+
+
+rm docker
+rm -rf dockercfg
